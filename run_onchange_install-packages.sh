553cb19305527358badcdaca74a28f880b568044
Fabian Zürker Aguilar <fzuerker@gmail.com>
2023-06-27 18:34:01 +0200

    Add check if omz already installed

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -2,8 +2,12 @@
 {{ if eq .chezmoi.os "linux" -}}
 sudo apt install zsh curl ripgrep
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 {{ else if eq .chezmoi.os "darwin" -}}
 brew install zsh curl ripgrep
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 {{ end -}}
+exec zsh
+if [[ -d "$HOME/.oh-my-zsh" ]]; then
+
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
