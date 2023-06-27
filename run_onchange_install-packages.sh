3f77efff06f1155bee9daf8411dc4662aa06bb46
Fabian Zürker Aguilar <fzuerker@gmail.com>
2023-06-27 18:27:16 +0200

    Move shebang

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,10 +1,9 @@
-{{ if eq .chezmoi.os "linux" -}}
 #!/bin/sh
+{{ if eq .chezmoi.os "linux" -}}
 sudo apt install zsh curl ripgrep
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 {{ else if eq .chezmoi.os "darwin" -}}
-#!/bin/sh
 brew install zsh curl ripgrep
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 {{ end -}}
